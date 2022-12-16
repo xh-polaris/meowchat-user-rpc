dcb0ff928a23ee7959b4006c56f7ff5397278752
foliet <foliet@rainspace.cn>
2022-12-16 23:06:16 +0800

    revert: 移除未使用的redis

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"github.com/zeromicro/go-zero/core/stores/cache"
-	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/zrpc"
 )
 
@@ -13,5 +12,4 @@ type Config struct {
 		DB  string
 	}
 	CacheConf cache.CacheConf
-	Redis     redis.RedisConf
 }
