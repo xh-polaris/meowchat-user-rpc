811ab8b9df2959849350ac2310f288b135aa092d
foliet <foliet@rainspace.cn>
2023-03-01 00:12:55 +0800

    refactor: 更改gRPC服务名

--- internal/server/user_rpc_server.go
+++ internal/server/user_rpc_server.go
@@ -11,28 +11,28 @@ import (
 	"github.com/xh-polaris/meowchat-user-rpc/pb"
 )
 
-type UserServer struct {
+type UserRpcServer struct {
 	svcCtx *svc.ServiceContext
-	pb.UnimplementedUserServer
+	pb.UnimplementedUserRpcServer
 }
 
-func NewUserServer(svcCtx *svc.ServiceContext) *UserServer {
-	return &UserServer{
+func NewUserRpcServer(svcCtx *svc.ServiceContext) *UserRpcServer {
+	return &UserRpcServer{
 		svcCtx: svcCtx,
 	}
 }
 
-func (s *UserServer) GetUser(ctx context.Context, in *pb.GetUserReq) (*pb.GetUserResp, error) {
+func (s *UserRpcServer) GetUser(ctx context.Context, in *pb.GetUserReq) (*pb.GetUserResp, error) {
 	l := logic.NewGetUserLogic(ctx, s.svcCtx)
 	return l.GetUser(in)
 }
 
-func (s *UserServer) UpdateUser(ctx context.Context, in *pb.UpdateUserReq) (*pb.UpdateUserResp, error) {
+func (s *UserRpcServer) UpdateUser(ctx context.Context, in *pb.UpdateUserReq) (*pb.UpdateUserResp, error) {
 	l := logic.NewUpdateUserLogic(ctx, s.svcCtx)
 	return l.UpdateUser(in)
 }
 
-func (s *UserServer) SearchUser(ctx context.Context, in *pb.SearchUserReq) (*pb.SearchUserResp, error) {
+func (s *UserRpcServer) SearchUser(ctx context.Context, in *pb.SearchUserReq) (*pb.SearchUserResp, error) {
 	l := logic.NewSearchUserLogic(ctx, s.svcCtx)
 	return l.SearchUser(in)
 }
